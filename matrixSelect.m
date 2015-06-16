4650231c2000234de9550913584914a3bf51b29e
Matthew Chang <m_chang@mit.edu>
2015-06-16 16:45:44 -0400

    Matrix select change made it too slow. Remade to be faster

--- matrixSelect.m
+++ matrixSelect.m
@@ -1,7 +1,17 @@
 function output = matrixSelect(A,sel)
-    sel_cell = num2cell(sel,1);
-    indices = sub2ind(size(A),sel_cell{:});
-    output = A(indices);
+%     sel_cell = num2cell(sel,1);
+%     indices = sub2ind(size(A),sel_cell{:});
+%     output = A(indices);
+
+    A_s = size(A);
+    muls = zeros(size(sel,2),1);
+    muls(1) = 1;
+    for i = 2:ndims(A)
+        muls(i) = muls(i-1)*A_s(i-1);
+    end
+    vals = ((sel-1)*muls)+1;
+    output = A(vals);
+
 %     A_s = size(A);
 %     muls = ones(ndims(A),1);
 %     for i = 2:ndims(A)
